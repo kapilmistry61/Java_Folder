589efc2ea55f6ec4676117cee953f99162511947
kapil kumar <32435747+kapilmistry61@users.noreply.github.com>
2021-06-16 17:38:17 +0530

    Create JavaMathClass.java

---
+++ JavaMathClass.java
@@ -0,0 +1,15 @@
+package mypack;
+import java.lang.Math;
+public class JavaMathClass{
+	public static void main(String args[])
+	{
+		int x=5;
+		int y=10;
+		System.out.println("addition of x and y is = "+Math.max(x,y));
+		System.out.println("minimum number is = "+Math.min(x,y));
+		System.out.println("maximum number is = "+Math.max(x,y));
+		System.out.println("square root of y is = "+Math.sqrt(y));
+		System.out.println("logarithm of x is = "+Math.log(x));
+		System.out.println("sine of x is = "+Math.sin(x));
+	}
+}
