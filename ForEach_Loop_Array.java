f86b1ca9db27d00f882a64e2454ff57d7ebc7f5c
kapil kumar <32435747+kapilmistry61@users.noreply.github.com>
2021-06-18 10:54:54 +0530

    Create ForEach_Loop_Array.java

---
+++ ForEach_Loop_Array.java
@@ -0,0 +1,11 @@
+package mypack;
+public class ForEach_Loop_Array{
+	public static void main(String args[])
+	{
+		int arr[] = {1,2,3,4,5};
+		for(int a:arr)
+		{
+			System.out.println(a);
+		}
+	}
+}
