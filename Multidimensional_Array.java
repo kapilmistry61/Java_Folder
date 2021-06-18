71cea21350d717ad42bd50b21a288f2d123879c3
kapil kumar <32435747+kapilmistry61@users.noreply.github.com>
2021-06-18 12:41:54 +0530

    Create Multidimensional_Array.java

---
+++ Multidimensional_Array.java
@@ -0,0 +1,15 @@
+package mypack;
+public class Multidimensional_Array{
+	public static void main(String args[])
+	{
+		int arr[][]= {{1,2,3},{4,5,4},{7,8,9}};
+		for(int i=0; i<3; i++)
+		{
+			for(int j=0; j<3; j++)
+			{
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
