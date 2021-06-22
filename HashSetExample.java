c22ab963e9c461f6874c6058185568203bed3c9d
kapil kumar <32435747+kapilmistry61@users.noreply.github.com>
2021-06-22 10:01:37 +0530

    Create HashSetExample.java

---
+++ HashSetExample.java
@@ -0,0 +1,34 @@
+package mypack;
+import java.util.HashSet;
+import java.util.Iterator;
+public class HashSetExample{
+	public static void main(String args[])
+	{
+		HashSet<String> hset=new HashSet<String>();
+		hset.add("amenda");
+		hset.add("carry");
+		hset.add("elon");
+		hset.add("akon");
+		hset.add("jussy");
+		//adding duplicate value
+		hset.add("amenda");
+		hset.add("jussy");
+		//adding null value
+		hset.add(null);
+		hset.add(null);
+		//iterating
+		Iterator<String> iterator=hset.iterator();
+		while(iterator.hasNext())
+		{
+			System.out.println(iterator.next());
+		}
+	}
+}
+/*output:
+null
+jussy
+akon
+amenda
+elon
+carry
+*/
