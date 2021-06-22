2f1df1f0e1c991ce9cfbaa6651a33fdfca2bdbcf
kapil kumar <32435747+kapilmistry61@users.noreply.github.com>
2021-06-22 10:13:01 +0530

    Create TreeSetExample.java

---
+++ TreeSetExample.java
@@ -0,0 +1,30 @@
+package mypack;
+import java.util.TreeSet;
+import java.util.Iterator;
+public class TreeSetExample{
+	public static void main(String args[])
+	{
+		TreeSet<String> tset=new TreeSet<String>();
+		tset.add("amenda");
+		tset.add("carry");
+		tset.add("elon");
+		tset.add("akon");
+		tset.add("jussy");
+		//adding duplicate value
+		tset.add("amenda");
+		tset.add("jussy");
+		//iterating
+		Iterator<String> iterator=tset.iterator();
+		while(iterator.hasNext())
+		{
+			System.out.println(iterator.next());
+		}
+	}
+}
+/*output:
+akon
+amenda
+carry
+elon
+jussy
+*/
