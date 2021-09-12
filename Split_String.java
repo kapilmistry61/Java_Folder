e520179db10d4c330681cb862bd6555d4b867875
kapil kumar <32435747+kapilmistry61@users.noreply.github.com>
2021-09-12 11:37:26 +0530

    Create Split_String.java
    spliting a string at a particuler potion

---
+++ Split_String.java
@@ -0,0 +1,10 @@
+class A{
+  public static void main(String[] args) {
+  String str = "word1 word2 word3 word4";
+  String subStr1 = str.substring(0,12);
+  String subStr2 = str.substring(12);
+
+  System.out.println(subStr1);
+  System.out.println(subStr2);
+}
+}
