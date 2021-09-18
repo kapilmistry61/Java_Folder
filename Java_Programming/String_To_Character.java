fdd82efbb99fcfb6b60b219dd191d2b4b8569c19
kapil kumar <32435747+kapilmistry61@users.noreply.github.com>
2021-09-18 17:53:56 +0530

    Create String_To_Character.java

---
+++ Java_Programming/String_To_Character.java
@@ -0,0 +1,10 @@
+class A{
+	public static void main(String[] args) {
+	    String str="hello";
+	    
+	    for(int i=0; i<str.length(); i++)
+	    {
+	    	System.out.println(str.charAt(i));
+	    }
+	}
+}
