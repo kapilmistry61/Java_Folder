c2906c8a3cc0d29195153932ead8f4bb77a8eb92
kapil kumar <32435747+kapilmistry61@users.noreply.github.com>
2021-09-18 09:18:21 +0530

    Update reverse_array.java
    reverse an array from specific number!

--- reverse_array.java
+++ reverse_array.java
@@ -1,29 +1,18 @@
 class A{
-    public static void main(String[] args) {
-        int arr[]={1,2,3,4,5,6,7,8,9,10};
+ public static void main(String[] args) {
+     int arr[]={1,2,3,4,5,6,7,8,9,10};
 
-        System.out.println("original array ");
-        for(int i=1; i<=arr.length; i++)
+        for(int a:arr)
         {
-            System.out.print(i);
-        }
-
-        System.out.println();
-
-        System.out.println("output pattarn like this ");
-        for(int k=1; k<=3; k++)
-        {
-            System.out.print(k);
-        }
+            if(a >3)
+            {
+                break;
+            }
+            System.out.print(a);
+        }     
         for(int j=arr.length; j>=4; j--)
         {
             System.out.print(j);
-        }
-    }
+        }    
+  }
 }
-/*OUTPUT
-original array 
-12345678910
-output pattarn like this 
-12310987654
-*/
