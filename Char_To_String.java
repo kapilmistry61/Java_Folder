7e7522ce37f96fefeda0ece118f437d795603368
kapil kumar <32435747+kapilmistry61@users.noreply.github.com>
2021-09-18 17:12:42 +0530

    Create Char_To_String.java

---
+++ Char_To_String.java
@@ -0,0 +1,11 @@
+class CharToString{
+	public static void main(String[] args) {
+		char chr='a';
+		//first method
+		String str=Character.toString(chr);
+        //second method
+        String str2=String.valueOf(chr);
+		System.out.println(str);
+		System.out.println(str2);
+	}
+}
